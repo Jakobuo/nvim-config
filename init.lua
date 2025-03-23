f535091c1ad1561da0940200cfcc715e7fda3e57
Jakobuo <github@jakobuo.com>
2025-03-23 14:23:46 +0100

    Added gitui-nvim

--- init.lua
+++ init.lua
@@ -284,6 +284,11 @@ require('lazy').setup({
     lazy = false,
   },
 
+  {
+    'mikinovation/nvim-gitui',
+    opts = {},
+    -- lazy = true,
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
